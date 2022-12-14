057c3da93810fa4f25aeaa5ff2741fbe92da7ca8
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-12-14 22:29:25 +0530

    added maximumkeyremovepolicycache

--- KeyRemoveCache.cpp
+++ KeyRemoveCache.cpp
@@ -24,20 +24,6 @@ class MinimumKeyRemovePolicyCache: public Cache
 		mp.clear();
 	}
 	
-	// MaximumKeyRemovePolicyCache(int _capacity)
-	// {
-	// 	capacity = _capacity; 
-	// 	mp.clear();
-	// }	
-
-	// int evict_key_and_return()
-	// {
-	// 	int key = mp.rbegin()->first; //iterator for key of map mp
-	// 	mp.erase(mp.rbegin()->first);
-	// 	// cout << "evicted is" << key << endl;
-	// 	return key;
-	// }
-
 	int evict_key_and_return()
 	{
 		int key = mp.begin()->first; //iterator for key of map mp
@@ -46,6 +32,55 @@ class MinimumKeyRemovePolicyCache: public Cache
 		return key;
 	}
 	
+	int get(int key)
+	{
+		if(mp.find(key) == mp.end())
+			return -1;
+		return mp[key];
+	}
+	
+	void put(int key, int value)
+	{
+		if(mp.find(key) != mp.end())
+		{
+			mp[key] = value;
+			return;
+		}
+
+		if(mp.size() < capacity)
+		{
+			mp[key] = value; //if key,value exists -> update key,new_value
+		}
+		else 
+		{ 
+				evict_key_and_return();
+				mp[key] = value;
+		}
+
+	}
+	
+	
+};
+
+class MaximumKeyRemovePolicyCache: public Cache
+{
+	public:
+		map<int, int> mp; //key,value
+
+	MaximumKeyRemovePolicyCache(int _capacity)
+	{
+		capacity = _capacity; 
+		mp.clear();
+	}	
+
+	int evict_key_and_return()
+	{
+		int key = mp.rbegin()->first; //iterator for key of map mp
+		mp.erase(mp.rbegin()->first);
+		// cout << "evicted is" << key << endl;
+		return key;
+	}
+
 	int get(int key)
 	{
 		if(mp.find(key) == mp.end())
@@ -84,6 +119,14 @@ int main() {
 	r.put(45, 1);
 	cout << r.get(-3) << endl;
 	cout << r.get(45) << endl;
+	
+	MaximumKeyRemovePolicyCache v = MaximumKeyRemovePolicyCache(2);
+	cout<< v.get(1) << endl;
+	v.put(3, 5);
+	v.put(1, 6);
+	v.put(4, 8);
+	cout << v.get(3) << endl;
+	cout << v.get(4) << endl;
 	
 	return 0;
 }
