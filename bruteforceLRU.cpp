65048d4c5571f4b99e9572beee57f0caf8c08863
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-12-24 22:29:30 +0530

    edited get() and put() functions

--- bruteforceLRU.cpp
+++ bruteforceLRU.cpp
@@ -1,4 +1,3 @@
-
 // DS used: vectors
 // implementing bruteforce LRU
 
@@ -17,16 +16,17 @@ class Cache
 
 };
 
-class bruteLRU: public Cache
+class BruteLRU: public Cache
 {
 	public:
   
-  vector<pair<int, int>>v; //vector of pairs
- 	bruteLRU(int _capacity)
+	vector<pair<int, int>>v; //vector of pairs
+ 	BruteLRU(int _capacity)
 	{
 		capacity = _capacity; 
 		v.clear();
-	}	
+	}
+	
 
 	int evict_key_and_return()
 	{
@@ -35,66 +35,65 @@ class bruteLRU: public Cache
 
 	int get(int key)
 	{
-    pair<int,int>ans(-1,-1);
-    for(int i=0;i <v.size(); i++) //max size of v = capacity
-    {
-      if(v[i].first == key)
-      {
-        ans = v[i];
-        v.erase(v.begin()+i);
-        v.push_back(ans);
-        break;
-      }
-    }
-    return ans.second;
+	    pair<int,int>ans(-1,-1);
+	    for(int i=0; i < v.size(); i++) //max size of v = capacity
+	    {
+	      if(v[i].first == key)
+	      {
+	        ans = v[i];
+	        v.erase(v.begin()+i);
+	        v.push_back(ans);
+	        break;
+	      }
+	    }
+	    return ans.second;
      
 	}
-	
-	void put(int k, int val)
+	void put(int key, int value)
 	{
-    int flag = 0;
-    pair<int,int>ans(-1,-1);
-	  for(int i=0; i <v.size(); i++)
-    {
-      if(v[i].first == key)
-      {
-        ans = v[i];
-        ans.second = val;
-        v.erase(v.begin()+i);
-        v.push_back(ans);
-      }
-      
-    }
-    
-    if(ans.first != -1)
-    {
-      return;
-    }
-    
-    if(v.size() = capacity)
-    {
-      v.erase(v.begin());
-      v.push_back(k,val);
-    }
-    else
-    {
-      v.push_back(k,val);
-    }
-      
+		pair<int, int> ans;
+		for(int i = 0; i < v.size(); i++)
+		{
+			
+			if(v[i].first == key)
+			{
+				ans = v[i];
+				v.erase(v.begin() + i);
+				ans.second = value;
+				v.push_back(ans);
+				return;
+			}
+		}
+		
+		if(v.size() == capacity)
+		{
+			v.erase(v.begin());
+			v.push_back({key, value});
+		}
+		else
+		{
+			v.push_back({key, value});
+		}
+		
+		return;
 	}
 	
 	
 };
+
 int main() 
 {
 	
-	MaximumKeyRemovePolicyCache v = MaximumKeyRemovePolicyCache(2);
-	cout<< v.get(1) << endl;
-	v.put(3, 5);
-	v.put(1, 6);
-	v.put(4, 8);
-	cout << v.get(3) << endl;
-	cout << v.get(4) << endl;
+	BruteLRU c = BruteLRU(2);
+	cout << c.get(1) << endl;
+	
+	c.put(3, 5);
+	c.put(1, 6);
+	c.put(4, 8);
+	cout << c.get(3) << endl;
+	cout << c.get(4) << endl;
+	
+	
 	
 	return 0;
 }
