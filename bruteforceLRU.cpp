36464a88e74a1144d08615e4b8294ba575b17ed0
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-12-24 23:33:24 +0530

    implementing modularity :)

--- bruteforceLRU.cpp
+++ bruteforceLRU.cpp
@@ -1,5 +1,11 @@
 // DS used: vectors
 // implementing bruteforce LRU
+// ----------------------------
+//         ASSUMPTIONS
+// ----------------------------
+// end = most recently used pair
+// front = least recently used pair
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -26,55 +32,50 @@ class BruteLRU: public Cache
 		capacity = _capacity; 
 		v.clear();
 	}
-	
-
-	int evict_key_and_return()
-	{
-		return -1;
-	}
-
-	int get(int key)
-	{
-	    pair<int,int>ans(-1,-1);
-	    for(int i=0; i < v.size(); i++) //max size of v = capacity
-	    {
-	      if(v[i].first == key)
-	      {
-	        ans = v[i];
-	        v.erase(v.begin()+i);
-	        v.push_back(ans);
-	        break;
-	      }
-	    }
-	    return ans.second;
-     
-	}
-	void put(int key, int value)
+	bool check_rearrange(int key)          //true = if the element is present & rearrange : false = not present
 	{
 		pair<int, int> ans;
-		for(int i = 0; i < v.size(); i++)
+
+		for(int i = 0; i < v.size(); i++)  //max size of v = capacity
 		{
-			
 			if(v[i].first == key)
 			{
 				ans = v[i];
-				v.erase(v.begin() + i);
-				ans.second = value;
-				v.push_back(ans);
-				return;
+				v.erase(v.begin() + i);   //deleting the ith entry (key,value) 
+				v.push_back(ans);         //positioning ans at the end 
+				return 1;
 			}
+		}
+		return 0; //invalid index
+	}
+	int evict_key_and_return()
+	{
+		int x = v[0].first;
+		v.erase(v.begin());
+		return x;
+	}
+ 
+	int get(int key)
+	{
+	    if( check_rearrange(key) == 1)
+	    	return v.back().second;
+		return -1;
+
+	}
+
+	void put(int key, int value)
+	{
+		pair<int, int> ans;
+		if(check_rearrange(key) == 1)
+		{
+			v.back().second = value;
+			return;
 		}
 		
 		if(v.size() == capacity)
-		{
-			v.erase(v.begin());
-			v.push_back({key, value});
-		}
-		else
-		{
-			v.push_back({key, value});
-		}
-		
+			evict_key_and_return();
+
+		v.push_back({key, value});
 		return;
 	}
 	
@@ -86,14 +87,10 @@ int main()
 	
 	BruteLRU c = BruteLRU(2);
 	cout << c.get(1) << endl;
-	
 	c.put(3, 5);
 	c.put(1, 6);
 	c.put(4, 8);
 	cout << c.get(3) << endl;
-	cout << c.get(4) << endl;
-	
-	
-	
+	cout << c.get(4) << endl;	
 	return 0;
 }
