6e8d06fb2bfefbde314117351fb59b27bac4a897
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-12-28 00:09:27 +0530

    optimised LRU cache code

--- LRUcache.cpp
+++ LRUcache.cpp
@@ -9,8 +9,80 @@ class Cache
 		int capacity;
 		//int size;
 		
-		virtual int evict_key_and_return(){}; 
+		virtual int evict_key_and_return(){}; //virtual [keyword] : compulsory for derived classes to implement
 		virtual int get(int key){};
 		virtual void put(int key, int value){};
 
 };
+
+class LRUcache: public Cache
+{
+	public:
+		map< int, list <pair<int,int>> :: iterator > mp;           //key, key address
+		list<pair<int,int>>l;   //doubly linked list 
+		
+	
+	LRUcache(int _capacity)
+	{
+		capacity = _capacity; 
+		mp.clear();
+	}
+	
+	
+	int evict_key_and_return()
+	{
+		pair<int,int>p = *l.begin();       //*l.begin() = key,value PAIR
+		l.erase(l.begin());
+		mp.erase(p.first);
+		return p.second;  //to find the last evicted element value
+	}
+	
+	int get(int key)
+	{
+		if(mp.find(key) == mp.end())
+			return -1;
+		auto a = mp[key];
+		pair<int,int>p = *a;
+		int val = p.second;
+		l.erase(a);
+		l.push_back({key, val});
+		mp[key] = l.rbegin();
+		return val;
+	}
+	
+	void put(int key, int value)
+	{
+		if(mp.find(key) != mp.end())
+		{
+			auto a = mp[key] ;
+			pair<int,int>p = *a;
+			l.erase(a);
+			l.push_back({key, value});
+			mp[key] = l.rbegin();
+			return;
+		}
+
+		if(mp.size() == capacity)
+			evict_key_and_return();
+			
+		l.push_back({key, value});
+		mp[key]= l.rbegin();  //adding new key, key address in map
+
+	}
+	
+	
+};
+
+int main() 
+{
+	
+	LRUcache lr = LRUcache(2);
+	cout<< lr.get(1) << endl;
+	lr.put(-3, 5);
+	lr.put(1, 4);
+	lr.put(45, 1);
+	cout << lr.get(-3) << endl;
+	cout << lr.get(45) << endl;
+	
+	return 0;
+}
