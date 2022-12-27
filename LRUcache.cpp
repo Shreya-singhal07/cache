d3f24f395e17e0b867879dd43a207d97c2e72dfc
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-12-28 00:31:08 +0530

    working on errors

--- LRUcache.cpp
+++ LRUcache.cpp
@@ -18,7 +18,7 @@ class Cache
 class LRUcache: public Cache
 {
 	public:
-		map< int, list <pair<int,int>> :: iterator > mp;           //key, key address
+		map<int,list<pair<int,int>>::iterator>mp;           //key, key address
 		list<pair<int,int>>l;   //doubly linked list 
 		
 	
@@ -46,7 +46,8 @@ class LRUcache: public Cache
 		int val = p.second;
 		l.erase(a);
 		l.push_back({key, val});
-		mp[key] = l.rbegin();
+		auto it= l.end();
+		mp[key] = --it;
 		return val;
 	}
 	
@@ -58,7 +59,8 @@ class LRUcache: public Cache
 			pair<int,int>p = *a;
 			l.erase(a);
 			l.push_back({key, value});
-			mp[key] = l.rbegin();
+			auto it= l.end();
+			mp[key] = --it;
 			return;
 		}
 
@@ -66,23 +68,26 @@ class LRUcache: public Cache
 			evict_key_and_return();
 			
 		l.push_back({key, value});
-		mp[key]= l.rbegin();  //adding new key, key address in map
+		auto it= l.end();
+		mp[key] = --it;  //adding new key, key address in map
 
 	}
-	
-	
+
 };
 
 int main() 
 {
 	
-	LRUcache lr = LRUcache(2);
+	LRUcache lr = LRUcache(3);
 	cout<< lr.get(1) << endl;
 	lr.put(-3, 5);
 	lr.put(1, 4);
+	lr.put(1, 7);
+	cout<< lr.get(1)<<endl;
 	lr.put(45, 1);
+	lr.put(7,1);
 	cout << lr.get(-3) << endl;
-	cout << lr.get(45) << endl;
+	cout << lr.get(7) << endl;
 	
 	return 0;
 }
